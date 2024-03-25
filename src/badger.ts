ba00c5a4ded0f5101bd14d01b0a777871862198e
Peter Cunha <petercunha8@gmail.com>
2024-03-24 18:06:54 -0700

    Add types

--- src/badger.ts
+++ src/badger.ts
@@ -1,21 +1,23 @@
+import { SearchResult } from './types';
+import puppeteer from 'puppeteer';
 import moment from 'moment';
-import * as puppeteer from 'puppeteer';
 
 async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://blog.twitch.tv/en/search/');
 
-    const results: any = await page.evaluate(`
+    const results = await await page.evaluate(`
+        // This code is run inside a headless browser via puppeteer
         import("/_pagefind/pagefind.js").then(pagefind => {
             return pagefind.search("badge", { sort: { date: "desc" } }).then(search => {
                 return Promise.all(search.results.map(hit => hit.data()))
             })
         })
-    `);
-
-    await results
-        // .filter((hit: any) => moment().diff(moment(new Date(hit.meta.dateFormatted)), 'days') < 400)
+    `) as SearchResult[];
+    
+    results
+        .filter((hit: any) => moment().diff(moment(new Date(hit.meta.dateFormatted)), 'days') < 30)
         .map((hit: any) => {
             const date = hit.meta.dateFormatted
             console.log(date)
---
+++ src/types.ts
@@ -0,0 +1,31 @@
+export type SearchResult = {
+    url: string,
+    excerpt: string,
+    content: string,
+    word_count: number,
+    filters: any,
+    meta: {
+      title: string,
+      image: string,
+      dateFormatted: string
+    },
+    anchors: any[],
+    weighted_locations: [
+        {
+            weight: number, 
+            balanced_score: number, 
+            location: number }
+    ],
+    locations: number[],
+    raw_content: string,
+    raw_url: string,
+    sub_results: [
+      {
+        title: string,
+        url: string,
+        weighted_locations: any[],
+        locations: any[],
+        excerpt: string
+      }
+    ]
+  }
